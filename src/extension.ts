efde120601df078266e15b90f7077dc1ecc8ba76
arsoedjono <arsoedjono@gmail.com>
2021-04-11 16:39:35 +0700

    remove logger

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,8 @@
 import * as vscode from 'vscode';
 
-export class Logger {
-	constructor(readonly output: vscode.OutputChannel) {
-		this.output = output
-	}
-
-	log(msg: string) {
-		let timestamp = new Date().toISOString().replace(/[a-zA-Z]/g, ' ').trim()
-		this.output.appendLine(`[${timestamp}] ${msg}`);
-	}
-
-	dispose() {
-		this.output.dispose();
-	}
-}
-
 export function activate(context: vscode.ExtensionContext) {
-	let logger = new Logger(vscode.window.createOutputChannel('EZ Env Variables'));
-	logger.log('starting extension');
-
 	let formatEnv = vscode.languages.registerDocumentFormattingEditProvider('ez-env-variables', {
 		provideDocumentFormattingEdits(doc) {
-			logger.log(`formatting ${doc.fileName}`)
-
 			let edits: vscode.TextEdit[] = [];
 
 			for (let i = 0; i < doc.lineCount; i++) {
@@ -37,7 +17,7 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	});
 
-	context.subscriptions.push(logger, formatEnv);
+	context.subscriptions.push(formatEnv);
 }
 
 export function deactivate() {}
