eedfaca1e8372b09d1f2add6df55de4769c59cda
arsoedjono <arsoedjono@gmail.com>
2021-04-11 17:24:37 +0700

    change file extension name and trim leading whitespace logic

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,17 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	let formatEnv = vscode.languages.registerDocumentFormattingEditProvider('ez-env-variables', {
+	let formatEnv = vscode.languages.registerDocumentFormattingEditProvider('dotenv', {
 		provideDocumentFormattingEdits(doc) {
 			let edits: vscode.TextEdit[] = [];
 
 			for (let i = 0; i < doc.lineCount; i++) {
 				let line = doc.lineAt(i);
+				let start = line.firstNonWhitespaceCharacterIndex
 
-				if (line.isEmptyOrWhitespace && line.text.length > 0) {
-					edits.push(vscode.TextEdit.delete(line.range));
+				// trim leading whitespace
+				if (start != 0) {
+					edits.push(vscode.TextEdit.delete(new vscode.Range(i, 0, i, start)));
 				}
 			}
 
