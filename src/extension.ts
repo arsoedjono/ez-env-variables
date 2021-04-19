4d15d2d0c0e93172e482cb140fe4b0d181038726
arsoedjono <arsoedjono@gmail.com>
2021-04-19 23:12:18 +0700

    reformat file

--- src/extension.ts
+++ src/extension.ts
@@ -1,75 +1,75 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	const formatEnv = vscode.languages.registerDocumentFormattingEditProvider('dotenv', {
-		provideDocumentFormattingEdits(doc) {
-			let edits: vscode.TextEdit[] = [];
+  const formatEnv = vscode.languages.registerDocumentFormattingEditProvider('dotenv', {
+    provideDocumentFormattingEdits(doc) {
+      let edits: vscode.TextEdit[] = [];
 
-			for (let i = 0; i < doc.lineCount; i++) {
-				const line = doc.lineAt(i);
-				let text = line.text;
+      for (let i = 0; i < doc.lineCount; i++) {
+        const line = doc.lineAt(i);
+        let text = line.text;
 
-				if (text.match(/^(?:# (?:\S|\S.*\S)|\w+=(?:\S|\S.*\S|))$/m)) {
-					continue;
-				}
+        if (text.match(/^(?:# (?:\S|\S.*\S)|\w+=(?:\S|\S.*\S|))$/m)) {
+          continue;
+        }
 
-				text = text.trim();
-				text = formatCommentRow(text);
-				text = formatEnvRow(text);
+        text = text.trim();
+        text = formatCommentRow(text);
+        text = formatEnvRow(text);
 
-				edits.push(vscode.TextEdit.replace(line.range, text));
-			}
+        edits.push(vscode.TextEdit.replace(line.range, text));
+      }
 
-			return edits;
-		}
-	});
+      return edits;
+    }
+  });
 
-	const fold = vscode.languages.registerFoldingRangeProvider('dotenv', {
-		provideFoldingRanges(doc) {
-			const folds: vscode.FoldingRange[] = [];
-			let start = -1;
+  const fold = vscode.languages.registerFoldingRangeProvider('dotenv', {
+    provideFoldingRanges(doc) {
+      const folds: vscode.FoldingRange[] = [];
+      let start = -1;
 
-			for (let i = 0; i < doc.lineCount; i++) {
-				const line = doc.lineAt(i);
-				const text = line.text;
+      for (let i = 0; i < doc.lineCount; i++) {
+        const line = doc.lineAt(i);
+        const text = line.text;
 
-				if (text[0] === '#') {
-					if (start >= 0) {
-						folds.push(new vscode.FoldingRange(start, i - 1, vscode.FoldingRangeKind.Region));
-					}
-					start = i;
-				} else if (line.isEmptyOrWhitespace) {
-					folds.push(new vscode.FoldingRange(start, i - 1, vscode.FoldingRangeKind.Region));
-					start = -1;
-				}
-			}
+        if (text[0] === '#') {
+          if (start >= 0) {
+            folds.push(new vscode.FoldingRange(start, i - 1, vscode.FoldingRangeKind.Region));
+          }
+          start = i;
+        } else if (line.isEmptyOrWhitespace) {
+          folds.push(new vscode.FoldingRange(start, i - 1, vscode.FoldingRangeKind.Region));
+          start = -1;
+        }
+      }
 
-			return folds;
-		}
-	});
+      return folds;
+    }
+  });
 
-	context.subscriptions.push(formatEnv, fold);
+  context.subscriptions.push(formatEnv, fold);
 }
 
 export function formatCommentRow(text: string): string {
-	if (text[0] !== '#') {
-		return text;
-	}
-	return `# ${text.substring(1).trim()}`.trim();
+  if (text[0] !== '#') {
+    return text;
+  }
+  return `# ${text.substring(1).trim()}`.trim();
 }
 
 export function formatEnvRow(text: string): string {
-	const matcher = text.match(/^\w+\s*(:|=)\s*/m);
+  const matcher = text.match(/^\w+\s*(:|=)\s*/m);
 
-	if (matcher === null) {
-		return text;
-	}
+  if (matcher === null) {
+    return text;
+  }
 
-	const separatorIdx = text.indexOf(matcher[1]);
-	const key = text.substring(0, separatorIdx).trim();
-	const value = text.substring(separatorIdx + 1).trim();
+  const separatorIdx = text.indexOf(matcher[1]);
+  const key = text.substring(0, separatorIdx).trim();
+  const value = text.substring(separatorIdx + 1).trim();
 
-	return `${key}=${value}`;
+  return `${key}=${value}`;
 }
 
-export function deactivate() {}
+export function deactivate() { }
