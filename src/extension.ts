02db521b9851b95da1f0e781cfb2b84978bd3f6f
arsoedjono <arsoedjono@gmail.com>
2021-05-08 09:54:41 +0700

    remove as and import as needed

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,22 @@
-import * as vscode from 'vscode';
+import { commands, ExtensionContext, languages } from 'vscode';
 import { Folder } from './lib/fold';
 import { Formatter } from './lib/formatter';
 import { valueFinder } from './lib/valueFinder';
 
-export function activate(context: vscode.ExtensionContext) {
-  const formatEnv = vscode.languages.registerDocumentFormattingEditProvider('dotenv', {
+export function activate(context: ExtensionContext) {
+  const formatEnv = languages.registerDocumentFormattingEditProvider('dotenv', {
     provideDocumentFormattingEdits(doc) {
       return new Formatter().start(doc);
     }
   });
 
-  const fold = vscode.languages.registerFoldingRangeProvider('dotenv', {
+  const fold = languages.registerFoldingRangeProvider('dotenv', {
     provideFoldingRanges(doc) {
       return new Folder().start(doc);
     }
   });
 
-  const findEnvValue = vscode.commands.registerCommand('ez-env-variables.findEnvValue', valueFinder);
+  const findEnvValue = commands.registerCommand('ez-env-variables.findEnvValue', valueFinder);
 
   context.subscriptions.push(formatEnv, fold, findEnvValue);
 }
